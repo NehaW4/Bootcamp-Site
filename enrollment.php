0a8709a1cf7c220b2dc32fb231c07bc6fd03fbb0
NehaW4 <92804162+NehaW4@users.noreply.github.com>
2024-05-18 13:39:58 +0530

    Add files via upload

---
+++ enrollment.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+</head>
+
+<body>
+	
+		<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => staff
+		$conn = mysqli_connect("localhost", "root", "", "enrollment");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$first_name = $_REQUEST['first_name'];
+		$last_name = $_REQUEST['last_name'];
+		
+		$email = $_REQUEST['email'];
+        $phone = $_REQUEST['phone'];
+		
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO enrollment VALUES ('$first_name', 
+			'$last_name','$email','$phone')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>"; 
+
+			echo nl2br("\n$first_name\n $last_name\n "
+				. "$email\n $phone");
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	
+</body>
+
+</html>
